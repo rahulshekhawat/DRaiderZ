f847eb27186cca3e22d45e15611b2a0228ea9950
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-06-21 19:05:56 +0530

    Add "EditorTools" editor module

---
+++ Source/EditorTools/Private/EditorTools.cpp
@@ -0,0 +1,13 @@
+#include "EditorTools.h"
+
+void FEditorToolsModule::StartupModule()
+{
+
+}
+
+void FEditorToolsModule::ShutdownModule()
+{
+
+}
+
+IMPLEMENT_GAME_MODULE(FEditorToolsModule, EditorTools);
