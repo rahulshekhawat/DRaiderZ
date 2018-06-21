034269f21ef24e9a14660fd6d46676329e123021
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-06-21 19:29:40 +0530

    Add AI character class and AI controller

---
+++ Source/EOD/AI/MobCharacter.h
@@ -0,0 +1,52 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/BaseCharacter.h"
+#include "MobCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API AMobCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+public:
+
+	AMobCharacter(const FObjectInitializer& ObjectInitializer);
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = AIMovement)
+	float MaxWalkSpeedInCombat;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = AIMovement)
+	float MaxWalkSpeedOutsideCombat;
+	
+	virtual void SetInCombat(bool bValue) override;
+
+	bool HasGreetingAnimation() const { bool bResult = GetGreetingMontageSectionName() == NAME_None ? false : true; return bResult; }
+
+	FName GetGreetingMontageSectionName() const { return GreetingMontageSectionName; }
+	
+protected:
+
+
+private:
+
+	/** Changes maximum walk speed of character based on whether character is engaged in combat or not */
+	void UpdateMaxWalkSpeed();
+	
+	UPROPERTY(EditDefaultsOnly, Category = Animations)
+	FName GreetingMontageSectionName;
+	
+
+
+};
---
+++ Source/EOD/AI/MobController.h
@@ -0,0 +1,20 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MobController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API AMobController : public AAIController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
