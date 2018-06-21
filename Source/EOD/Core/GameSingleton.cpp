adc20a5ea95b6cb84d4a233c1c6164125283c916
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-06-21 19:29:52 +0530

    Add singleton class

---
+++ Source/EOD/Core/GameSingleton.cpp
@@ -0,0 +1,8 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#include "GameSingleton.h"
+
+UGameSingleton::UGameSingleton(const FObjectInitializer & ObjectInitializer): Super(ObjectInitializer)
+{
+
+}
