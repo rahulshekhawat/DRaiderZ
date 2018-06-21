8de59cea2ac05f2a9b35e22ade3d0921c9cac853
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-06-21 19:26:11 +0530

    Add base character class with multiplayer support

---
+++ Source/EOD/Player/BaseCharacter.cpp
@@ -0,0 +1,312 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#include "BaseCharacter.h"
+#include "CharAnimInstance.h"
+#include "Components/BaseStatsComponent.h"
+
+#include "UnrealNetwork.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values
+ABaseCharacter::ABaseCharacter(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Initialize Stats Component
+	StatsComp = ObjectInitializer.CreateDefaultSubobject<UBaseStatsComponent>(this, FName("Character Stats Component"));
+	
+	// Initialize variables
+	CharacterState = ECharacterState::IdleWalkRun;
+
+}
+
+// Called every frame
+void ABaseCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ABaseCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME_CONDITION(ABaseCharacter, CharacterState, COND_SkipOwner);
+
+}
+
+bool ABaseCharacter::IsAlive() const
+{
+	return StatsComp->CurrentHealth > 0;
+}
+
+bool ABaseCharacter::IsDead() const
+{
+	return StatsComp->CurrentHealth <= 0;
+}
+
+bool ABaseCharacter::IsIdle() const
+{
+	return (CharacterState == ECharacterState::IdleWalkRun && GetVelocity().Size() == 0);
+}
+
+bool ABaseCharacter::IsMoving() const
+{
+	return (CharacterState == ECharacterState::IdleWalkRun && GetVelocity().Size() != 0);
+}
+
+bool ABaseCharacter::IsJumping() const
+{
+	return CharacterState == ECharacterState::Jumping;
+}
+
+bool ABaseCharacter::IsBlocking() const
+{
+	return CharacterState == ECharacterState::Blocking;
+}
+
+bool ABaseCharacter::IsCastingSpell() const
+{
+	return CharacterState == ECharacterState::CastingSpell;
+}
+
+bool ABaseCharacter::IsNormalAttacking() const
+{
+	return CharacterState == ECharacterState::Attacking;
+}
+
+bool ABaseCharacter::IsDodging() const
+{
+	return CharacterState == ECharacterState::Dodging;
+}
+
+bool ABaseCharacter::NeedsHeal() const
+{
+	return StatsComp->CurrentHealth / StatsComp->MaxHealth <= 0.25;
+}
+
+void ABaseCharacter::SetCharacterState(ECharacterState NewState)
+{
+	CharacterState = NewState;
+
+	if (Role < ROLE_Authority)
+	{
+		Server_SetCharacterState(NewState);
+	}
+}
+
+void ABaseCharacter::Server_SetCharacterState_Implementation(ECharacterState NewState)
+{
+	SetCharacterState(NewState);
+}
+
+bool ABaseCharacter::Server_SetCharacterState_Validate(ECharacterState NewState)
+{
+	return true;
+}
+
+void ABaseCharacter::SetWalkSpeed(float WalkSpeed)
+{
+	GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+	
+	if (Role < ROLE_Authority)
+	{
+		Server_SetWalkSpeed(WalkSpeed);
+	}
+}
+
+void ABaseCharacter::SetCharacterRotation(FRotator NewRotation)
+{
+	GetCharacterMovement()->FlushServerMoves();
+	SetActorRotation(NewRotation);
+
+	if (Role < ROLE_Authority)
+	{
+		Server_SetCharacterRotation(NewRotation);
+	}
+}
+
+void ABaseCharacter::SetUseControllerRotationYaw(bool bNewBool)
+{
+	bUseControllerRotationYaw = bNewBool;
+
+	if (Role < ROLE_Authority)
+	{
+		Server_SetUseControllerRotationYaw(bNewBool);
+	}
+}
+
+void ABaseCharacter::SetNextMontageSection(FName CurrentSection, FName NextSection)
+{
+	if (GetMesh()->GetAnimInstance())
+	{
+		GetMesh()->GetAnimInstance()->Montage_SetNextSection(CurrentSection, NextSection);
+	}
+
+	Server_SetNextMontageSection(CurrentSection, NextSection);
+}
+
+void ABaseCharacter::Server_SetNextMontageSection_Implementation(FName CurrentSection, FName NextSection)
+{
+
+	Multicast_SetNextMontageSection(CurrentSection, NextSection);
+}
+
+bool ABaseCharacter::Server_SetNextMontageSection_Validate(FName CurrentSection, FName NextSection)
+{
+	return true;
+}
+
+void ABaseCharacter::Server_SetUseControllerRotationYaw_Implementation(bool bNewBool)
+{
+	SetUseControllerRotationYaw(bNewBool);
+}
+
+bool ABaseCharacter::Server_SetUseControllerRotationYaw_Validate(bool bNewBool)
+{
+	return true;
+}
+
+void ABaseCharacter::Server_SetCharacterRotation_Implementation(FRotator NewRotation)
+{
+	SetCharacterRotation(NewRotation);
+}
+
+bool ABaseCharacter::Server_SetCharacterRotation_Validate(FRotator NewRotation)
+{
+	return true;
+}
+
+void ABaseCharacter::Server_SetWalkSpeed_Implementation(float WalkSpeed)
+{
+	SetWalkSpeed(WalkSpeed);
+}
+
+bool ABaseCharacter::Server_SetWalkSpeed_Validate(float WalkSpeed)
+{
+	return true;
+}
+
+/*
+void ABaseCharacter::PlayAnimationMontage(UAnimMontage * MontageToPlay)
+{
+	if (GetMesh()->GetAnimInstance())
+	{
+		GetMesh()->GetAnimInstance()->Montage_Play(MontageToPlay);
+	}
+
+	Server_PlayAnimationMontage(MontageToPlay);
+}
+*/
+
+void ABaseCharacter::Multicast_SetNextMontageSection_Implementation(FName CurrentSection, FName NextSection)
+{
+	FString Message = FString("Multi cast called");
+	UKismetSystemLibrary::PrintString(this, Message, true, false);
+
+	if (!IsLocallyControlled() && GetMesh()->GetAnimInstance())
+	{
+		GetMesh()->GetAnimInstance()->Montage_SetNextSection(CurrentSection, NextSection);
+	}
+}
+
+void ABaseCharacter::PlayAnimationMontage(UAnimMontage * MontageToPlay, FName SectionToPlay, ECharacterState NewState)
+{
+	if (GetMesh()->GetAnimInstance())
+	{
+		GetMesh()->GetAnimInstance()->Montage_Play(MontageToPlay);
+		GetMesh()->GetAnimInstance()->Montage_JumpToSection(SectionToPlay);
+		CharacterState = NewState;
+	}
+
+	Server_PlayAnimationMontage(MontageToPlay, SectionToPlay, NewState);
+}
+
+void ABaseCharacter::Server_PlayAnimationMontage_Implementation(UAnimMontage * MontageToPlay, FName SectionToPlay, ECharacterState NewState)
+{
+	MultiCast_PlayAnimationMontage(MontageToPlay, SectionToPlay, NewState);
+}
+
+bool ABaseCharacter::Server_PlayAnimationMontage_Validate(UAnimMontage * MontageToPlay, FName SectionToPlay, ECharacterState NewState)
+{
+	return true;
+}
+
+/*
+void ABaseCharacter::Server_PlayAnimationMontage_Implementation(UAnimMontage * MontageToPlay)
+{
+	MultiCast_PlayAnimationMontage(MontageToPlay);
+}
+
+bool ABaseCharacter::Server_PlayAnimationMontage_Validate(UAnimMontage * MontageToPlay)
+{
+	return true;
+}
+
+void ABaseCharacter::MultiCast_PlayAnimationMontage_Implementation(UAnimMontage * MontageToPlay)
+{
+	if (GetMesh()->GetAnimInstance())
+	{
+		GetMesh()->GetAnimInstance()->Montage_Play(MontageToPlay);
+	}
+}
+*/
+
+void ABaseCharacter::MultiCast_PlayAnimationMontage_Implementation(UAnimMontage * MontageToPlay, FName SectionToPlay, ECharacterState NewState)
+{
+	if (!IsLocallyControlled() && GetMesh()->GetAnimInstance())
+	{
+		GetMesh()->GetAnimInstance()->Montage_Play(MontageToPlay);
+		GetMesh()->GetAnimInstance()->Montage_JumpToSection(SectionToPlay);
+		CharacterState = NewState;
+	}
+}
+
+bool ABaseCharacter::CanMove() const
+{
+	// Mobs can only move in IdleWalkRun state
+	return CharacterState == ECharacterState::IdleWalkRun;
+}
+
+bool ABaseCharacter::CanJump() const
+{
+	return CharacterState == ECharacterState::IdleWalkRun;
+}
+
+bool ABaseCharacter::CanBlock() const
+{
+	return CharacterState == ECharacterState::IdleWalkRun;
+}
+
+bool ABaseCharacter::CanRespawn() const
+{
+	return false;
+}
+
+bool ABaseCharacter::CanNormalAttack() const
+{
+	return CharacterState == ECharacterState::IdleWalkRun;
+}
+
+bool ABaseCharacter::CanDodge() const
+{
+	return CharacterState == ECharacterState::IdleWalkRun;
+}
+
+// Called when the game starts or when spawned
+void ABaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Source/EOD/Player/BaseCharacter.h
@@ -0,0 +1,134 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Statics/CharacterLibrary.h"
+#include "GameFramework/Character.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS()
+class EOD_API ABaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	// Sets default values for this character's properties
+	ABaseCharacter(const FObjectInitializer& ObjectInitializer);
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+	// Property replication
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	//~ Begin Character Action State Handlers
+	
+	virtual bool IsAlive() const;
+
+	virtual bool IsDead() const;
+	
+	virtual bool IsIdle() const;
+
+	virtual bool IsMoving() const;
+
+	virtual bool IsJumping() const;
+
+	virtual bool IsDodging() const;	
+	
+	virtual bool IsBlocking() const;
+
+	virtual bool IsCastingSpell() const;
+
+	virtual bool IsNormalAttacking() const;
+
+	virtual bool CanMove() const;
+	
+	virtual bool CanJump() const;
+
+	virtual bool CanDodge() const;
+	
+	virtual bool CanBlock() const;
+
+	virtual bool CanRespawn() const;
+
+	virtual bool CanNormalAttack() const;
+	
+	// Playing special idle animations through Random Sequece Player seems like a better idea
+	// virtual bool IsPlayingSpecialIdle() const;
+
+	//~ End Character Action State Handlers
+
+
+	virtual void SetInCombat(bool bValue) { bInCombat = bValue; }
+
+	// virtual void SetCombatState
+	//~ TODO : Combat State: Damaged, running, waiting, engaged with enemy, healing etc.?
+
+	//~ Begin Character Combat Status Functionality
+	virtual bool NeedsHeal() const;
+	//~ End Character Combat Status Functionality
+
+
+	//~ Begin Multiplayer Code
+	void SetCharacterState(ECharacterState NewState);
+	
+	UPROPERTY(Replicated)
+	ECharacterState CharacterState;
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetCharacterState(ECharacterState NewState);
+
+	void SetWalkSpeed(float WalkSpeed);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetWalkSpeed(float WalkSpeed);
+
+	void SetCharacterRotation(FRotator NewRotation);
+
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetCharacterRotation(FRotator NewRotation);
+
+	void SetUseControllerRotationYaw(bool bNewBool);
+	
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetUseControllerRotationYaw(bool bNewBool);
+
+	void SetNextMontageSection(FName CurrentSection, FName NextSection);
+	
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_SetNextMontageSection(FName CurrentSection, FName NextSection);
+	
+	UFUNCTION(NetMultiCast, Reliable)
+	void Multicast_SetNextMontageSection(FName CurrentSection, FName NextSection);
+
+	UFUNCTION(BlueprintCallable, Category = Animations)
+	void PlayAnimationMontage(class UAnimMontage* MontageToPlay, FName SectionToPlay, ECharacterState NewState);
+	
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_PlayAnimationMontage(class UAnimMontage* MontageToPlay, FName SectionToPlay, ECharacterState NewState);
+	
+	UFUNCTION(NetMultiCast, Reliable)
+	void MultiCast_PlayAnimationMontage(class UAnimMontage* MontageToPlay, FName SectionToPlay, ECharacterState NewState);
+	//~ End Multiplayer Code
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	/** Determines whether character is currently engaged in combat or not. Not in combat by default. */
+	// UPROPERTY(replicated)
+	bool bInCombat = false;	
+
+private:
+	
+	UPROPERTY(Category = Character, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UBaseStatsComponent* StatsComp;
+	
+
+};
