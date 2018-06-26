0e96c611f7a31849f3792735d4a656ba364c716e
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-06-26 13:11:42 +0530

    Change hard animation references to soft references in data table

--- Source/EOD/Statics/CharacterLibrary.cpp
+++ Source/EOD/Statics/CharacterLibrary.cpp
@@ -3,6 +3,10 @@
 #include "CharacterLibrary.h"
 #include "Core/GameSingleton.h"
 
+#include "Engine/StreamableManager.h"
+#include "Animation/AnimMontage.h"
+#include "GenericPlatform/GenericPlatformProcess.h"
+
 UCharacterLibrary::UCharacterLibrary(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
 {
 }
@@ -14,37 +18,68 @@ FPlayerAnimationReferences * UCharacterLibrary::GetPlayerAnimationReferences(EWe
 	if (GEngine && GEngine->GameSingleton)
 	{
 		UGameSingleton* GameSingleton = Cast<UGameSingleton>(GEngine->GameSingleton);
-
+		FPlayerAnimationSoftReferences* PlayerAnimationSoftReferences = nullptr;
+		
 		switch (PlayerWeaponAnimationType)
 		{
 		case EWeaponAnimationType::NoWeapon:
-			PlayerAnimationReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationReferences>(FName("NoWeapon"), FString("Animation references for no weapon equipped"));
+			PlayerAnimationSoftReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationSoftReferences>(FName("NoWeapon"), FString("Animation references for no weapon equipped"));
 			break;
 		case EWeaponAnimationType::SheathedWeapon:
-			PlayerAnimationReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationReferences>(FName("SheathedWeapon"), FString("Animation references when weapon is sheathed"));
+			PlayerAnimationSoftReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationSoftReferences>(FName("SheathedWeapon"), FString("Animation references when weapon is sheathed"));
 			break;
 		case EWeaponAnimationType::ShieldAndSword:
-			PlayerAnimationReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationReferences>(FName("ShieldAndSword"), FString("Animation references for Shield and Sword"));
+			PlayerAnimationSoftReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationSoftReferences>(FName("ShieldAndSword"), FString("Animation references for Shield and Sword"));
 			break;
 		case EWeaponAnimationType::ShieldAndMace:
-			PlayerAnimationReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationReferences>(FName("ShieldAndMace"), FString("Animation references for Shield and Mace"));
+			PlayerAnimationSoftReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationSoftReferences>(FName("ShieldAndMace"), FString("Animation references for Shield and Mace"));
 			break;
 		case EWeaponAnimationType::GreatSword:
-			PlayerAnimationReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationReferences>(FName("GreatSword"), FString("Animation references for GreatSword"));
+			PlayerAnimationSoftReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationSoftReferences>(FName("GreatSword"), FString("Animation references for GreatSword"));
 			break;
 		case EWeaponAnimationType::WarHammer:
-			PlayerAnimationReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationReferences>(FName("WarHammer"), FString("Animation references for WarHammer"));
+			PlayerAnimationSoftReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationSoftReferences>(FName("WarHammer"), FString("Animation references for WarHammer"));
 			break;
 		case EWeaponAnimationType::Staff:
-			PlayerAnimationReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationReferences>(FName("Staff"), FString("Animation references for Staff"));
+			PlayerAnimationSoftReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationSoftReferences>(FName("Staff"), FString("Animation references for Staff"));
 			break;
 		case EWeaponAnimationType::Daggers:
-			PlayerAnimationReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationReferences>(FName("Daggers"), FString("Animation references for Daggers"));
+			PlayerAnimationSoftReferences = GameSingleton->PlayerAnimationReferencesDataTable->FindRow<FPlayerAnimationSoftReferences>(FName("Daggers"), FString("Animation references for Daggers"));
 			break;
 		default:
 			break;
 		}
+
+		//@ todo Modify following code to load assets asynchronously
+		if (PlayerAnimationSoftReferences)
+		{
+			PlayerAnimationReferences = new FPlayerAnimationReferences;
+			PlayerAnimationReferences->AnimationMontage_Dodge = GameSingleton->StreamableManager.LoadSynchronous<UAnimMontage>(PlayerAnimationSoftReferences->Dodge);
+			PlayerAnimationReferences->AnimationMontage_Jump = GameSingleton->StreamableManager.LoadSynchronous<UAnimMontage>(PlayerAnimationSoftReferences->Jump);
+			PlayerAnimationReferences->AnimationMontage_NormalAttacks = GameSingleton->StreamableManager.LoadSynchronous<UAnimMontage>(PlayerAnimationSoftReferences->NormalAttacks);
+			PlayerAnimationReferences->AnimationMontage_SpecialActions = GameSingleton->StreamableManager.LoadSynchronous<UAnimMontage>(PlayerAnimationSoftReferences->SpecialActions);
+			PlayerAnimationReferences->AnimationMontage_Skills = GameSingleton->StreamableManager.LoadSynchronous<UAnimMontage>(PlayerAnimationSoftReferences->Skills);
+			PlayerAnimationReferences->AnimationMontage_Spells = GameSingleton->StreamableManager.LoadSynchronous<UAnimMontage>(PlayerAnimationSoftReferences->Spells);
+			PlayerAnimationReferences->AnimationMontage_SpecialMovement = GameSingleton->StreamableManager.LoadSynchronous<UAnimMontage>(PlayerAnimationSoftReferences->SpecialMovement);
+			PlayerAnimationReferences->AnimationMontage_CrowdControlEffects = GameSingleton->StreamableManager.LoadSynchronous<UAnimMontage>(PlayerAnimationSoftReferences->CrowdControlEffects);
+			PlayerAnimationReferences->AnimationMontage_Flinch = GameSingleton->StreamableManager.LoadSynchronous<UAnimMontage>(PlayerAnimationSoftReferences->Flinch);
+
+		}
 	}
 
 	return PlayerAnimationReferences;
+}
+
+FPlayerAnimationReferences::~FPlayerAnimationReferences()
+{
+	AnimationMontage_Jump = nullptr;
+	AnimationMontage_Dodge = nullptr;
+	AnimationMontage_NormalAttacks = nullptr;
+	AnimationMontage_SpecialActions = nullptr;
+	AnimationMontage_Skills = nullptr;
+	AnimationMontage_Spells = nullptr;
+	AnimationMontage_SpecialMovement = nullptr;
+	AnimationMontage_CrowdControlEffects = nullptr;
+	AnimationMontage_Flinch = nullptr;
+
 }
