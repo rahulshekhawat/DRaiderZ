13209a1d16690cdc8269dfe88b3784a1f1d52ba2
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-07-04 00:56:17 +0530

    Remove stats variables from BaseStatsComponent

--- Source/EOD/Player/Components/BaseStatsComponent.h
+++ Source/EOD/Player/Components/BaseStatsComponent.h
@@ -136,21 +136,6 @@ public:
 
 	virtual int32 GetDarkness() const PURE_VIRTUAL(UBaseStatsComponent::GetDarkness, return 0; );
 
-/*
-* PhysicalCritRate
-* MagickCritRate
-
-* PhysicalCritBonus
-* MagickCritBonus
-
-* ElementalFireResistance
-* ElementalIceResistance
-* ElementalLightningResistance
-* ElementalPoisonResistance
-* ElementalHolyResistance
-* ElementalDarkResistance
-*/
-
 protected:
 
 	UPROPERTY(EditDefaultsOnly, Category = Conditionals)
@@ -162,78 +147,6 @@ protected:
 	UPROPERTY(EditDefaultsOnly, Category = Conditionals)
 	uint32 bHasStaminaRegenration : 1;
 
-public:
-
-	/** Character Level */
-	UPROPERTY(EditDefaultsOnly, Category = BaseStats)
-	int32 Level;
-	
-	// UPROPERTY(Replicated, EditDefaultsOnly, Category = StatusIndicator)
-	UPROPERTY(EditDefaultsOnly, Category = BaseStats)
-	int32 Darkness;
-
-	/** Maximum health of character without any status effects */
-	UPROPERTY(EditDefaultsOnly, Category = BaseStats)
-	int32 BaseHealth;
-
-	/** Current maximum health of character - with or without any status effects */
-	UPROPERTY(Replicated)
-	int32 MaxHealth;
-
-	/** Current health of character */
-	UPROPERTY(Replicated)
-	int32 CurrentHealth;
-
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = OffensiveStats)
-	int32 PhysicalAttack;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = DefensiveStats)
-	int32 PhysicalResistance;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = OffensiveStats)
-	int32 MagickAttack;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = DefensiveStats)
-	int32 MagickResistance;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = BaseStats, AdvancedDisplay)
-	int32 HealthRegenRate;	
-
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = OffensiveStats, AdvancedDisplay)
-	int32 ElementalFireDamage;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = OffensiveStats, AdvancedDisplay)
-	int32 ElementalIceDamage;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = OffensiveStats, AdvancedDisplay)
-	int32 ElementalPoisonDamage;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = OffensiveStats, AdvancedDisplay)
-	int32 ElementalLightningDamage;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = OffensiveStats, AdvancedDisplay)
-	int32 ElementalHolyDamage;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = OffensiveStats, AdvancedDisplay)
-	int32 ElementalDarkDamage;
-
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = DefensiveStats, AdvancedDisplay)
-	int32 ElementalFireResistance;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = DefensiveStats, AdvancedDisplay)
-	int32 ElementalIceResistance;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = DefensiveStats, AdvancedDisplay)
-	int32 ElementalPoisonResistance;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = DefensiveStats, AdvancedDisplay)
-	int32 ElementalLightningResistance;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = DefensiveStats, AdvancedDisplay)
-	int32 ElementalHolyResistance;
-	
-	UPROPERTY(Replicated, EditDefaultsOnly, Category = DefensiveStats, AdvancedDisplay)
-	int32 ElementalDarkResistance;
 		
 	
 };
