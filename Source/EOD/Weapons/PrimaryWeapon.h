a47712b1cebcb6d8326af203a369506fb908123b
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-07-16 21:19:22 +0530

    Update documentation for PrimaryWeapon and fix attachment

--- Source/EOD/Weapons/PrimaryWeapon.h
+++ Source/EOD/Weapons/PrimaryWeapon.h
@@ -8,6 +8,9 @@
 
 class USkeletalMeshComponent;
 
+/**
+ Determines whether primary weapon is equipped in single hand or both hands
+ */
 UENUM(BlueprintType)
 enum class EPrimaryWeaponType : uint8
 {
@@ -16,7 +19,7 @@ enum class EPrimaryWeaponType : uint8
 };
 
 /**
- * 
+ * A PrimaryWeapon is a either a dual handed weapon or a single handed weapon that can be equipped in right hand
  */
 UCLASS()
 class EOD_API APrimaryWeapon : public ABaseWeapon
@@ -24,27 +27,34 @@ class EOD_API APrimaryWeapon : public ABaseWeapon
 	GENERATED_BODY()
 	
 public:
-	// Sets default values for this actor's properties
-	APrimaryWeapon(const FObjectInitializer& ObjectInitializer);
 
-	// Called every frame
+	/** Constructor is used to create this weapon's default skeletal mesh component objects */
+	APrimaryWeapon(const FObjectInitializer& ObjectInitializer);
+	
+	/** Dummy declaration. This actor doesn't tick */
 	virtual void Tick(float DeltaTime) override;
 	
+	/** Called when this weapon is equipped by a character */
 	virtual void OnEquip(FWeaponData* NewWeaponData) override;
-
+	
+	/** Called when this weapon is unequipped by a character */
 	virtual void OnUnEquip() override;	
 
 private:
-
+	
+	/** Mesh component that represents the mesh for weapon equipped in left hand */
 	UPROPERTY(Category = Weapon, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	USkeletalMeshComponent* LeftHandWeaponMeshComp;
 		
+	/** Mesh component that represents the mesh for weapon equipped in right hand */
 	UPROPERTY(Category = Weapon, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	USkeletalMeshComponent* RightHandWeaponMeshComp;
 		
+	/** Mesh component that represents the mesh for the weapon that has fallen on ground */
 	UPROPERTY(Category = Weapon, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	USkeletalMeshComponent* FallenWeaponMeshComp;
 	
+	/** Mesh component that represents the mesh for sheathed weapon */
 	UPROPERTY(Category = Weapon, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	USkeletalMeshComponent* SheathedWeaponMeshComp;
 
