78cf9445ec92a8cecfbdffbe51f849cef881b9b2
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-07-16 21:01:31 +0530

    Update documentation for SecondaryWeapon

--- Source/EOD/Weapons/SecondaryWeapon.h
+++ Source/EOD/Weapons/SecondaryWeapon.h
@@ -7,7 +7,7 @@
 #include "SecondaryWeapon.generated.h"
 
 /**
- * 
+ * A SecondaryWeapon is a single handed weapon that can be equipped in left hand
  */
 UCLASS()
 class EOD_API ASecondaryWeapon : public ABaseWeapon
@@ -15,24 +15,30 @@ class EOD_API ASecondaryWeapon : public ABaseWeapon
 	GENERATED_BODY()
 	
 public:
-	// Sets default values for this actor's properties
+
+	/** Constructor is used to create this weapon's default skeletal mesh component objects */
 	ASecondaryWeapon(const FObjectInitializer& ObjectInitializer);
 	
-	// Called every frame
+	/** Dummy declaration. This actor doesn't tick */
 	virtual void Tick(float DeltaTime) override;
 	
+	/** Called when this weapon is equipped by a character */
 	virtual void OnEquip(FWeaponData* NewWeaponData) override;
 
+	/** Called when this weapon is unequipped by a character */
 	virtual void OnUnEquip() override;
 	
 private:
 
+	/** Mesh component that represents the mesh for weapon equipped in left hand */
 	UPROPERTY(Category = Weapon, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	USkeletalMeshComponent* LeftHandWeaponMeshComp;
 		
+	/** Mesh component that represents the mesh for the weapon that has fallen on ground */
 	UPROPERTY(Category = Weapon, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	USkeletalMeshComponent* FallenWeaponMeshComp;
 	
+	/** Mesh component that represents the mesh for sheathed weapon */
 	UPROPERTY(Category = Weapon, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	USkeletalMeshComponent* SheathedWeaponMeshComp;
 	
