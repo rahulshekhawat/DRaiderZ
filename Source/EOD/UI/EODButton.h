e7a7603f4f39973b6ce573615d2cb5c8b93ffe43
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-07-16 11:08:23 +0530

    Add UI classes

---
+++ Source/EOD/UI/EODButton.h
@@ -0,0 +1,24 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Button.h"
+#include "EODButton.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API UEODButton : public UButton
+{
+	GENERATED_BODY()
+	
+public:
+
+	UEODButton(const FObjectInitializer& ObjectInitializer);
+	
+
+
+
+};
---
+++ Source/EOD/UI/InventoryWidget.h
@@ -0,0 +1,20 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InventoryWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API UInventoryWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
