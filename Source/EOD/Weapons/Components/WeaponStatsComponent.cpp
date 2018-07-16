4e6bebce52bba614e5325313569e8fbe71d7174f
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-07-17 01:05:26 +0530

    Update documentation for WeaponStatsComponent

--- Source/EOD/Weapons/Components/WeaponStatsComponent.cpp
+++ Source/EOD/Weapons/Components/WeaponStatsComponent.cpp
@@ -6,29 +6,20 @@
 // Sets default values for this component's properties
 UWeaponStatsComponent::UWeaponStatsComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
+	// This component doesn't tick
 	PrimaryComponentTick.bCanEverTick = false;
 
-	// ...
 }
 
-
-// Called when the game starts
 void UWeaponStatsComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
 }
 
-
-// Called every frame
 void UWeaponStatsComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
 }
 
