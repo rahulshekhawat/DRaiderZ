858c3160a56150ed02e2e33179034043763689ed
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-07-21 16:33:54 +0530

    Add enum for crowd control effects to DamageLibrary

--- Source/EOD/Statics/DamageLibrary.h
+++ Source/EOD/Statics/DamageLibrary.h
@@ -6,6 +6,19 @@
 #include "UObject/NoExportTypes.h"
 #include "DamageLibrary.generated.h"
 
+
+/** This enum describes all the possible crowd control effects in-game */
+UENUM(BlueprintType, Meta = (BitFlags))
+enum class ECrowdControlEffect : uint8
+{
+	Flinch,
+	Interrupt,
+	KnockedDown,
+	KnockedBack,
+	Stunned,
+	Crystalized
+};
+
 /**
  * DamageLibrary contains static helper functions for handling in-game damage events.
  * @note Do not derive from this class
