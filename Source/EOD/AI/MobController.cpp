f2f8e128b368e2f1b2fc4ef04d545c2700bacdfc
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-08-23 15:24:30 +0530

    Move blackboard keys initialization to c++ from blueprints

--- Source/EOD/AI/MobController.cpp
+++ Source/EOD/AI/MobController.cpp
@@ -1,7 +1,28 @@
 // Copyright 2018 Moikkai Games. All Rights Reserved.
 
 #include "MobController.h"
+#include "Statics/AILibrary.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+AMobController::AMobController(const FObjectInitializer & ObjectInitializer)
+{
+	AggroActivationRadius = 500;
+	AggroAreaRadius = 5000;
+	MaxEnemyChaseRadius = 1000;
+	WanderRadius = 3500;
+}
 
+void AMobController::InitializeBlackboardValues(UBlackboardComponent * BlackboardComponent)
+{
+	if (!BlackboardComponent)
+	{
+		return;
+	}
 
+	BlackboardComponent->SetValueAsVector(UAILibrary::BBKey_SpawnLocation, GetPawn()->GetActorLocation());
+	BlackboardComponent->SetValueAsFloat(UAILibrary::BBKey_AggroActivationRadius, AggroActivationRadius);
+	BlackboardComponent->SetValueAsFloat(UAILibrary::BBKey_MaxEnemyChaseRadius, MaxEnemyChaseRadius);
+	BlackboardComponent->SetValueAsFloat(UAILibrary::BBKey_AggroAreaRadius, AggroAreaRadius);
+	BlackboardComponent->SetValueAsFloat(UAILibrary::BBKey_WanderRadius, WanderRadius);
 
+}
