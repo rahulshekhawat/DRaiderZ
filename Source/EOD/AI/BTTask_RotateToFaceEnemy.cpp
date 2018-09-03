96061759077d1c7e255a674df5ab72a12fc18cda
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-09-03 23:54:04 +0530

    Add BTTask to rotate AI to face it's enemy

---
+++ Source/EOD/AI/BTTask_RotateToFaceEnemy.cpp
@@ -0,0 +1,51 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#include "BTTask_RotateToFaceEnemy.h"
+#include "Player/EODCharacterBase.h"
+#include "Statics/AILibrary.h"
+
+#include "AIController.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+EBTNodeResult::Type UBTTask_RotateToFaceEnemy::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	AAIController* AIController = Cast<AAIController>(OwnerComp.GetOwner());
+	AEODCharacterBase* OwningCharacter = Cast<AEODCharacterBase>(AIController->GetPawn());
+	AEODCharacterBase* TargetEnemy = Cast<AEODCharacterBase>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(UAILibrary::BBKey_TargetEnemy));
+
+	FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(OwningCharacter->GetActorLocation(), TargetEnemy->GetActorLocation());
+	FRotator ActorRotation = OwningCharacter->GetActorRotation();
+
+	bool bResult = OwningCharacter->DeltaRotateCharacterToDesiredYaw(LookAtRotation.Yaw, 0.f, Precision);
+	if (bResult)
+	{
+		return EBTNodeResult::Succeeded;
+	}
+	else
+	{
+		return EBTNodeResult::InProgress;
+	}
+}
+
+void UBTTask_RotateToFaceEnemy::TickTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory, float DeltaSeconds)
+{
+	AAIController* AIController = Cast<AAIController>(OwnerComp.GetOwner());
+	AEODCharacterBase* OwningCharacter = Cast<AEODCharacterBase>(AIController->GetPawn());
+	AEODCharacterBase* TargetEnemy = Cast<AEODCharacterBase>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(UAILibrary::BBKey_TargetEnemy));
+
+	FRotator LookAtRotation = UKismetMathLibrary::FindLookAtRotation(OwningCharacter->GetActorLocation(), TargetEnemy->GetActorLocation());
+	FRotator ActorRotation = OwningCharacter->GetActorRotation();
+
+	bool bResult = OwningCharacter->DeltaRotateCharacterToDesiredYaw(LookAtRotation.Yaw, 0.f, Precision);
+	if (bResult)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	}
+}
+
+EBTNodeResult::Type UBTTask_RotateToFaceEnemy::AbortTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
+{
+	return EBTNodeResult::Type();
+}
---
+++ Source/EOD/AI/BTTask_RotateToFaceEnemy.h
@@ -0,0 +1,31 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_RotateToFaceEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API UBTTask_RotateToFaceEnemy : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+
+	/** Success condition precision in degrees */
+	UPROPERTY(EditDefaultsOnly, Category = Node)
+	float Precision;
+
+};
