643097a63511fa6b22182f3a7a81c5794ae1c0c8
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-09-05 00:50:44 +0530

    Replace the class for AI rotation BTTask

--- Source/EOD/AI/BTTask_EODRotateToFaceBBEntry.h
+++ Source/EOD/AI/BTTask_EODRotateToFaceBBEntry.h
@@ -3,31 +3,32 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "BehaviorTree/BTTaskNode.h"
-#include "BTTask_RotateToFaceEnemy.generated.h"
+#include "UObject/ObjectMacros.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "BTTask_EODRotateToFaceBBEntry.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class EOD_API UBTTask_RotateToFaceEnemy : public UBTTaskNode
+class EOD_API UBTTask_EODRotateToFaceBBEntry : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
 	
 public:
 
-	UBTTask_RotateToFaceEnemy(const FObjectInitializer& ObjectInitializer);
+	UBTTask_EODRotateToFaceBBEntry(const FObjectInitializer& ObjectInitializer);
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 
 	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 
 	virtual EBTNodeResult::Type AbortTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
-
+	
 protected:
 
 	/** Success condition precision in degrees */
-	UPROPERTY(EditAnywhere, Category = Node)
+	UPROPERTY(EditAnywhere, Category = Node, meta = (ClampMin = "0.0"))
 	float Precision;
-
+	
 };
