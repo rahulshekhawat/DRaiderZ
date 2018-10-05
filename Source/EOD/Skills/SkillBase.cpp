9fe3a4c9acb1748791a092ea31f0c506063e8140
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-10-05 12:11:18 +0530

    Add base skill class

---
+++ Source/EOD/Skills/SkillBase.cpp
@@ -0,0 +1,9 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#include "SkillBase.h"
+
+USkillBase::USkillBase(const FObjectInitializer & ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+
---
+++ Source/EOD/Skills/SkillBase.h
@@ -0,0 +1,26 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SkillBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API USkillBase : public UDataAsset
+{
+	GENERATED_BODY()
+	
+public:
+
+	USkillBase(const FObjectInitializer& ObjectInitializer);
+
+
+
+	
+	
+	
+};
