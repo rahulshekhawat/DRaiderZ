16d313915680f385c7fb64971a2e27860db102d1
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-10-07 21:16:56 +0530

    Add skill manager

---
+++ Source/EOD/Core/SkillsManager.cpp
@@ -0,0 +1,21 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#include "SkillsManager.h"
+
+ASkillsManager::ASkillsManager(const FObjectInitializer & ObjectInitializer) : Super(ObjectInitializer)
+{
+}
+
+void ASkillsManager::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ASkillsManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+
+}
