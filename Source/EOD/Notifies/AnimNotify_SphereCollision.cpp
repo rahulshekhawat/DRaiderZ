fd0638320f3a8a8078e12e71a8859df992a2b0e8
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-10-17 20:50:27 +0530

    Modify spher collision notify to use combat manager

--- Source/EOD/Notifies/AnimNotify_SphereCollision.cpp
+++ Source/EOD/Notifies/AnimNotify_SphereCollision.cpp
@@ -3,6 +3,8 @@
 #include "AnimNotify_SphereCollision.h"
 #include "Statics/CombatLibrary.h"
 #include "Core/EODPreprocessors.h"
+#include "Core/CombatZoneModeBase.h"
+#include "Core/CombatManager.h"
 #include "Player/EODCharacterBase.h"
 
 
@@ -11,7 +13,7 @@
 
 void UAnimNotify_SphereCollision::Notify(USkeletalMeshComponent * MeshComp, UAnimSequenceBase * Animation)
 {
-	// only call from server
+	// only process this notify if called from server (dedicated or listen)
 	if (!MeshComp->GetOwner() || MeshComp->GetOwner()->GetNetMode() == NM_Client)
 	{
 		return;
@@ -21,24 +23,20 @@ void UAnimNotify_SphereCollision::Notify(USkeletalMeshComponent * MeshComp, UAni
 	FVector TransformedCenter = WorldTransform.TransformPosition(Center);
 	
 #if WITH_EDITOR
-	
 	UKismetSystemLibrary::DrawDebugSphere(MeshComp, TransformedCenter, Radius, 12, FLinearColor::White, 5.f, 1.f);
-
 #endif // WITH_EDITOR
-	
-	FCollisionQueryParams Params = UCombatLibrary::GenerateCombatCollisionQueryParams(MeshComp->GetOwner());
+
+	// Only process this notify if the current game mode is ACombatZoneModeBase
+	ACombatZoneModeBase* CombatZoneGameMode = Cast<ACombatZoneModeBase>(MeshComp->GetWorld()->GetAuthGameMode());
+	if (!CombatZoneGameMode)
+	{
+		return;
+	}
 
 	FCollisionShape CollisionShape = FCollisionShape::MakeSphere(Radius);
+	FCollisionQueryParams Params = UCombatLibrary::GenerateCombatCollisionQueryParams(MeshComp->GetOwner());
 	TArray<FHitResult> HitResults;
 	
 	bool bHit = MeshComp->GetWorld()->SweepMultiByChannel(HitResults, TransformedCenter, TransformedCenter, FQuat::Identity, COLLISION_COMBAT, CollisionShape, Params);
-	AEODCharacterBase* BaseCharacter = Cast<AEODCharacterBase>(MeshComp->GetOwner());
-	if (BaseCharacter)
-	{
-		UCombatLibrary::HandleCombatCollision(BaseCharacter, Animation, HitResults, bHit);
-	}
-	else
-	{
-		UCombatLibrary::HandleCombatCollision(MeshComp->GetOwner(), Animation, HitResults, bHit);
-	}
+	CombatZoneGameMode->GetCombatManager()->OnMeleeAttack(MeshComp->GetOwner(), bHit, HitResults);
 }
