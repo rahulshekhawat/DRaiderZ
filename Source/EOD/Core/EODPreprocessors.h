a63a0285c1e8bce9e02a50a3ead77acf9b46f7e7
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-10-31 13:23:51 +0530

    Implement AnimNotify for player footstep sounds

--- Source/EOD/Core/EODPreprocessors.h
+++ Source/EOD/Core/EODPreprocessors.h
@@ -7,3 +7,15 @@
 #define TEST_CODE_ENABLED 1
 #define DEBUG_SHAPES_ENABLED 0
 #define MESSAGE_LOGGING_ENABLED 1
+
+#define SURFACETYPE_FLESH       SurfaceType1
+#define SURFACETYPE_METAL       SurfaceType2
+#define SURFACETYPE_STONE       SurfaceType3
+#define SURFACETYPE_UNDEAD      SurfaceType4
+#define SURFACETYPE_WOOD        SurfaceType5
+#define SURFACETYPE_WATER       SurfaceType6
+#define SURFACETYPE_SNOW        SurfaceType7
+#define SURFACETYPE_SAND        SurfaceType8
+#define SURFACETYPE_FOLIAGE     SurfaceType9
+#define SURFACETYPE_DIRT        SurfaceType10
+#define SURFACETYPE_CONCRETE    SurfaceType11
---
+++ Source/EOD/Notifies/AnimNotify_FootStepBase.h
@@ -0,0 +1,98 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/EngineTypes.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_FootStepBase.generated.h"
+
+class USoundBase;
+class USoundAttenuation;
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API UAnimNotify_FootStepBase : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+
+	// virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation) override;
+
+	/** Determines if this notify is called when character's left foot hits the ground */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = FootNotify)
+	bool bLeftFoot;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* DefaultLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* DefaultRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* StoneLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* StoneRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* ConcreteLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* ConcreteRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* DirtLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* DirtRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* MetalLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* MetalRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* FoliageLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* FoliageRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* SandLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* SandRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* SnowLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* SnowRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* WaterLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* WaterRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* WoodLeftFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundBase* WoodRightFootSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Sounds)
+	USoundAttenuation* SoundAttenuation;
+
+	UFUNCTION(BlueprintCallable, Category = FootNotify)
+	void GetFootstepHitResults(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, FHitResult& OutHitResults);
+
+	UFUNCTION(BlueprintCallable, Category = FootNotify)
+	void PlayFootstepSound(const FHitResult& HitResult);
+
+};
