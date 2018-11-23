d907ee8e2f3a32d7fd3d636ef19c94a194d15ba8
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-11-23 17:07:30 +0530

    Add dialogue data tables

--- Source/EOD/Statics/DialogueLibrary.h
+++ Source/EOD/Statics/DialogueLibrary.h
@@ -3,8 +3,56 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Engine/DataTable.h"
 #include "UObject/NoExportTypes.h"
 #include "DialogueLibrary.generated.h"
+
+
+UENUM(BlueprintType)
+enum EEventType
+{
+	Dialogue,	// Triggers a new dialogue
+	Option,		// Replaces current dialogue options with new options
+	ArmorMenu,	// Opens armor craft menu
+	WeaponMenu,	// Opens weapon craft menu
+	Exit,		// Exits dialogue. Implies the interacting player wants to finish dialogue.
+	Finish,		// Same as Exit event but it implies that the dialogue sequence is over.
+};
+
+USTRUCT(BlueprintType)
+struct EOD_API FDialogueOptions : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
+	FString OptionText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
+	TEnumAsByte<EEventType> EventType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue Option")
+	FName EventID;
+
+};
+
+/** Struct for in-game skills */
+USTRUCT(BlueprintType)
+struct EOD_API FDialogueWindow : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue")
+	FString DialogueText;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Dialogue", meta = (DisplayName = "Option IDs"))
+	TArray<FName> OptionIDs;
+
+};
+
 
 /**
  * 
