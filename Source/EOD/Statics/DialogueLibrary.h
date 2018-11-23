bab8ae6eee30ddbfa06b0587f5141a1ec28ed922
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-11-23 13:34:19 +0530

    Add dialogue library class

---
+++ Source/EOD/Statics/DialogueLibrary.h
@@ -0,0 +1,17 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "DialogueLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API UDialogueLibrary : public UObject
+{
+	GENERATED_BODY()
+	
+};
