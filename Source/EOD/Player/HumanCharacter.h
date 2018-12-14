0b7498ade1de3029b44066a8174abbbbcd1e7a27
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2018-12-14 08:33:00 +0530

    Add new character classes

---
+++ Source/EOD/Player/HumanCharacter.h
@@ -0,0 +1,17 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/EODCharacterBase.h"
+#include "HumanCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API AHumanCharacter : public AEODCharacterBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/EOD/Player/RideCharacter.h
@@ -0,0 +1,17 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/EODCharacterBase.h"
+#include "RideCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API ARideCharacter : public AEODCharacterBase
+{
+	GENERATED_BODY()
+	
+};
