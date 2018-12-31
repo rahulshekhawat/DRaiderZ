ce5614b3dedf6c677c19244b4b5ad4b0a589c489
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2019-01-01 02:00:44 +0530

    Add a global names class

---
+++ Source/EOD/Statics/EODGlobalNames.cpp
@@ -0,0 +1,9 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#include "EOD/Statics/EODGlobalNames.h"
+
+const FName FEODGlobalNames::BaseColor(TEXT("BaseColor"));
+const FName FEODGlobalNames::NoCollision(TEXT("NoCollision"));
+const FName FEODGlobalNames::OverlapAllDynamic(TEXT("OverlapAllDynamic"));
+
+const FName FEODKeyNames::LeftMouseButton(TEXT("LeftMouseButton"));
---
+++ Source/EOD/Statics/EODGlobalNames.h
@@ -0,0 +1,19 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+class FEODGlobalNames
+{
+public:
+	const static FName BaseColor;
+	const static FName NoCollision;
+	const static FName OverlapAllDynamic;
+};
+
+class FEODKeyNames
+{
+public:
+	const static FName LeftMouseButton;
+};
