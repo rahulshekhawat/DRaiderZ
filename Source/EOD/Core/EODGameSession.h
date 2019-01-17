8d7119a4c297759f300a61a51b04743d0a435309
rahulshekhawat <rahul.shekhawat.dev.mail@gmail.com>
2019-01-18 02:42:08 +0530

    Add IP based matchmaking

---
+++ Source/EOD/Core/EODGameSession.h
@@ -0,0 +1,17 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameSession.h"
+#include "EODGameSession.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOD_API AEODGameSession : public AGameSession
+{
+	GENERATED_BODY()
+	
+};
