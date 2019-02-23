b960b4203fff1ebf4bfc27cae505e942e5d01036
rahulshekhawat <rahul.shekhawat.dev.mail@gmail.com>
2019-02-23 17:02:26 +0530

    Implement flying

--- Source/EOD/Characters/RideBase.cpp
+++ Source/EOD/Characters/RideBase.cpp
@@ -2,6 +2,8 @@
 
 #include "RideBase.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
 
 ARideBase::ARideBase(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
@@ -12,7 +14,52 @@ ARideBase::ARideBase(const FObjectInitializer& ObjectInitializer) : Super(Object
 	}
 }
 
+void ARideBase::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+}
+
 void ARideBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+
+}
+
+void ARideBase::Restart()
+{
+	Super::Restart();
+
+	//~ @note Setting the movement mode in ::Restart() because setting the movement mode to flying in ::BeginPlay() or ::PostInitializeComponents() just resets it back to MOVE_Walking
+	if (bCanFly)
+	{
+		UCharacterMovementComponent* MoveComp = GetCharacterMovement();
+		if (MoveComp)
+		{
+			MoveComp->SetMovementMode(EMovementMode::MOVE_Flying);
+		}
+	}
+
+}
+
+void ARideBase::MoveForward(const float Value)
+{
+	ForwardAxisValue = Value;
+
+	if (Value != 0 && CanMove())
+	{
+		FVector Direction = GetControlRotation().Vector();
+		AddMovementInput(Direction , Value);
+	}
+}
+
+void ARideBase::MoveRight(const float Value)
+{
+	RightAxisValue = Value;
+
+	if (Value != 0 && CanMove())
+	{
+		FVector Direction = FRotationMatrix(GetControlRotation()).GetScaledAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
 }
--- Source/EOD/Characters/RideBase.h
+++ Source/EOD/Characters/RideBase.h
@@ -17,18 +17,34 @@ class EOD_API ARideBase : public AEODCharacterBase
 public:
 	ARideBase(const FObjectInitializer& ObjectInitializer);
 
+	virtual void PostInitializeComponents() override;
+
 	/** Called when the game starts or when spawned */
 	virtual void BeginPlay() override;
 
+	/** Called when the Pawn is being restarted (usually by being possessed by a Controller). Called on both server and owning client. */
+	virtual void Restart() override;
 
 	////////////////////////////////////////////////////////////////////////////////
 	// Ride System
 public:
 
 protected:
+	/** Determines whether this ride can fly */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ride System")
+	uint32 bCanFly : 1;
 
 private:
 
+
+	////////////////////////////////////////////////////////////////////////////////
+	// Movement
+public:
+	/** Move character forward/backward */
+	virtual void MoveForward(const float Value);
+
+	/** Move character left/right */
+	virtual void MoveRight(const float Value);
 
 
 
