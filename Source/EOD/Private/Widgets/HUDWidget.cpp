a06e5387bf9d5ce58bc8512f3c239b129f906246
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2019-07-22 06:29:19 +0530

    Update HUDWidget

--- Source/EOD/Private/Widgets/HUDWidget.cpp
+++ Source/EOD/Private/Widgets/HUDWidget.cpp
@@ -2,6 +2,24 @@
 
 
 #include "HUDWidget.h"
+#include "SkillBarWidget.h"
+#include "InventoryWidget.h"
+#include "PlayerStatsWidget.h"
+#include "SkillTreeWidget.h"
+#include "DialogueWindowWidget.h"
+#include "ContainerDragDropOperation.h"
+#include "ContainerWidget.h"
+#include "NotificationWidget.h"
+#include "EODPlayerController.h"
+#include "GameplayEffectBase.h"
+#include "InteractivePopupWidget.h"
+
+#include "TimerManager.h"
+#include "Engine/World.h"
+#include "Components/TextBlock.h"
+#include "Components/CanvasPanel.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Kismet/GameplayStatics.h"
 
 UHUDWidget::UHUDWidget(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
@@ -9,8 +27,22 @@ UHUDWidget::UHUDWidget(const FObjectInitializer& ObjectInitializer) : Super(Obje
 
 bool UHUDWidget::Initialize()
 {
-	if (Super::Initialize())
+	if (Super::Initialize() &&
+		MainCanvas &&
+		StatusIndicatorWidget &&
+		InventoryWidget &&
+		PlayerStatsWidget &&
+		SkillTreeWidget &&
+		SkillBarWidget &&
+		PlayerLevel &&
+		PlayerName &&
+		InteractivePopup)
 	{
+		InteractivePopup->SetVisibility(ESlateVisibility::Hidden);
+		SkillTreeWidget->SetVisibility(ESlateVisibility::Hidden);
+		InventoryWidget->SetVisibility(ESlateVisibility::Hidden);
+		PlayerStatsWidget->SetVisibility(ESlateVisibility::Hidden);
+
 		return true;
 	}
 
@@ -25,4 +57,16 @@ void UHUDWidget::NativeConstruct()
 void UHUDWidget::NativeDestruct()
 {
 	Super::NativeDestruct();
+}
+
+void UHUDWidget::SetPlayerLevel(int32 Level)
+{
+	check(PlayerLevel);
+	PlayerLevel->SetText(FText::FromString(FString("Lv. ") + FString::FromInt(Level)));
+}
+
+void UHUDWidget::SetPlayerName(FString Name)
+{
+	check(PlayerName);
+	PlayerName->SetText(FText::FromString(Name));
 }
--- Source/EOD/Public/Widgets/HUDWidget.h
+++ Source/EOD/Public/Widgets/HUDWidget.h
@@ -6,6 +6,15 @@
 #include "Blueprint/UserWidget.h"
 #include "HUDWidget.generated.h"
 
+class UTextBlock;
+class UCanvasPanel;
+class USkillBarWidget;
+class UInventoryWidget;
+class USkillTreeWidget;
+class UPlayerStatsWidget;
+class UStatusIndicatorWidget;
+class UInteractivePopupWidget;
+
 /**
  * 
  */
@@ -14,10 +23,9 @@ class EOD_API UHUDWidget : public UUserWidget
 {
 	GENERATED_BODY()
 
-public:
-
 	///////////////////////////////////////////////////////////////////////////
 	//  UE4 Method Overrides
+public:
 
 	UHUDWidget(const FObjectInitializer& ObjectInitializer);
 
@@ -27,10 +35,51 @@ public:
 
 	virtual void NativeDestruct() override;
 
+
+	///////////////////////////////////////////////////////////////////////////
+	//  Child Widgets
 protected:
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (BindWidget))
-	class USkillBarWidget* SkillBarWidget;
-
 	
+	/** The main canvas that contains all the child widgets */
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UCanvasPanel* MainCanvas;
+
+	/** Widget containing health, mana, and stamina bars */
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UStatusIndicatorWidget* StatusIndicatorWidget;
+
+	/** Widget containing player stats info */
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UPlayerStatsWidget* PlayerStatsWidget;
+
+	/** Widget containing player inventory items */
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UInventoryWidget* InventoryWidget;
+
+	/** Widget containing skill trees of all vocations */
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	USkillTreeWidget* SkillTreeWidget;
+	
+	/** Widget containing skills that the player can actively use */
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	USkillBarWidget* SkillBarWidget;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UInteractivePopupWidget* InteractivePopup;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UTextBlock* PlayerLevel;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UTextBlock* PlayerName;
+	
+public:
+	
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	void SetPlayerLevel(int32 Level);
+
+	UFUNCTION(BlueprintCallable, Category = "Utility")
+	void SetPlayerName(FString Name);
+
+
 };
