dede3efb4f1d7343f35d492836ba75d01c2637d8
Rahul Shekhawat <rahul.shekhawat.dev.mail@gmail.com>
2019-12-07 12:10:18 +0530

    Add inventory interface

---
+++ Source/EOD/Private/Items/InventoryInterface.cpp
@@ -0,0 +1,11 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#include "InventoryInterface.h"
+
+UInventoryInterface::UInventoryInterface(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{	
+}
+
+void IInventoryInterface::OnClick_Implementation(AEODCharacterBase* ClickingChar) const
+{
+}
---
+++ Source/EOD/Public/Items/InventoryInterface.h
@@ -0,0 +1,38 @@
+// Copyright 2018 Moikkai Games. All Rights Reserved.
+
+#pragma once
+
+#include "UObject/Interface.h"
+#include "InventoryInterface.generated.h"
+
+class AEODCharacterBase;
+
+/** An interface that must be implemented for all in-game items that can be put in inventory */
+UINTERFACE(BlueprintType)
+class EOD_API UInventoryInterface : public UInterface
+{
+	GENERATED_BODY()
+
+public:
+
+    UInventoryInterface(const FObjectInitializer& ObjectInitializer);
+
+};
+
+/** An interface that must be implemented for all in-game items that can be put in inventory */
+class EOD_API IInventoryInterface
+{
+	GENERATED_BODY()
+	
+public:
+
+	// --------------------------------------
+	//  Inventory Interface
+	// --------------------------------------
+
+	/** This event is called when the inventory item is clicked in inventory */
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = Inventory)
+	void OnClick(AEODCharacterBase* ClickingChar) const;
+	virtual void OnClick_Implementation(AEODCharacterBase* ClickingChar) const;
+
+};
